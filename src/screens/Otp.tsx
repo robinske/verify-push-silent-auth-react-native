08d473427b1fb3bfdc9868f56c5aa43862bb5bfa
Kelley Robinson <3673341+robinske@users.noreply.github.com>
2022-01-28 16:14:53 -0500

    Replace OTP library

--- src/screens/Otp.tsx
+++ src/screens/Otp.tsx
@@ -3,11 +3,12 @@ import { SafeAreaView, StyleSheet, Button, Text } from "react-native";
 import commonStyles from "../styles/common-styles";
 
 import { checkVerification } from "../api/verify";
-import OTPInputView from "@twotalltotems/react-native-otp-input";
+import OtpInputs from "react-native-otp-inputs";
 
 const Otp = ({ route, navigation }) => {
   const { phoneNumber } = route.params;
   const [invalidCode, setInvalidCode] = useState(false);
+  const inputLength = 6;
   return (
     <SafeAreaView style={commonStyles.wrapper}>
       <Text style={commonStyles.prompt}>Enter the code we sent you</Text>
@@ -18,17 +19,20 @@ const Otp = ({ route, navigation }) => {
         title="Edit Phone Number"
         onPress={() => navigation.replace("PhoneNumber")}
       />
-      <OTPInputView
-        style={{ width: "80%", height: 200 }}
-        pinCount={6}
-        autoFocusOnLoad
-        codeInputFieldStyle={styles.underlineStyleBase}
-        codeInputHighlightStyle={styles.underlineStyleHighLighted}
-        onCodeFilled={(code) => {
-          checkVerification(phoneNumber, code).then((success) => {
-            if (!success) setInvalidCode(true);
-            success && navigation.replace("RegisterPush", { phoneNumber });
-          });
+      <OtpInputs
+        placeholder="7"
+        style={styles.otpForm}
+        inputStyles={styles.otpText}
+        inputContainerStyles={styles.pinContainer}
+        selectTextOnFocus={true}
+        numberOfInputs={inputLength}
+        handleChange={(code) => {
+          if (code.length === inputLength) {
+            checkVerification(phoneNumber, code).then((success) => {
+              if (!success) setInvalidCode(true);
+              success && navigation.replace("RegisterPush", { phoneNumber });
+            });
+          }
         }}
       />
       {invalidCode && <Text style={styles.error}>Incorrect code.</Text>}
@@ -37,26 +41,23 @@ const Otp = ({ route, navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  borderStyleBase: {
-    width: 30,
-    height: 45,
+  otpForm: {
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 
-  borderStyleHighLighted: {
-    borderColor: "#03DAC6",
-  },
-
-  underlineStyleBase: {
-    width: 30,
-    height: 45,
-    borderWidth: 0,
-    borderBottomWidth: 1,
+  otpText: {
     color: "black",
     fontSize: 20,
+    textAlign: "center",
   },
 
-  underlineStyleHighLighted: {
-    borderColor: "#03DAC6",
+  pinContainer: {
+    width: 30,
+    height: 30,
+    borderWidth: 0,
+    borderBottomWidth: 1,
+    margin: 10,
   },
 
   error: {
